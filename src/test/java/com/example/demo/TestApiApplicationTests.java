f4168c2ee751e1f18580eb337a3de40795e3cc0a
Radosław Lach <108222128+Lachuuuu@users.noreply.github.com>
2025-05-04 14:23:54 +0200

    fail build

--- src/test/java/com/example/demo/TestApiApplicationTests.java
+++ src/test/java/com/example/demo/TestApiApplicationTests.java
@@ -3,11 +3,18 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.Assert.assertTrue;
+
 @SpringBootTest
 class TestApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void test() {
+		assertTrue(false);
 	}
 
 }
